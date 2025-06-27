b6b530b6e8581de7cbb770b446b7f6ec2e03846b
Henrique Monteiro <offy.monteiro@gmail.com>
2025-06-27 15:54:32 -0300

    feat: adiciona geração de senha aleatória com input dinâmico do usuário
    Implementa uma função que gera senhas aleatórias com base na quantidade de caracteres definida pelo usuário. A senha é composta por letras (maiúsculas e minúsculas), números e símbolos especiais, garantindo maior segurança.

--- password_generator.py
+++ password_generator.py
@@ -1,8 +1,28 @@
 import random
 import string 
 
-def password_generator(Len_pass = 8)
-    ascii_options = string.ascci_letters
+def password_generator(Len_pass=8):
+    ascii_options = string.ascii_letters
     number_options = string.digits
     punt_options = string.punctuation
     options = ascii_options + number_options + punt_options
+
+    password_user = ""
+
+    for i in range (0, Len_pass):
+        digit = random.choice(options)
+        password_user = password_user + digit
+
+    return password_user
+
+choice_user = input('Quantos digitos na senha? R: ')
+
+if choice_user.isdigit():
+    choice_user = int(choice_user)
+    response = password_generator(Len_pass= choice_user)
+    print(f'Senha Gerada: \n{response}')
+else:
+    print('Entrada Inválida!')
+    quit()
+
+    
